6688d5550b9f10b4aff7919da1f949d481710134
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-05-07 15:44:42 -0300

    conclusão do projeto base

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,25 +5,25 @@ import Document, {
   Head,
   Main,
   NextScript,
-} from "next/document";
+} from "next/document"
 
-import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
   static async getInitialProps(
-    ctx: DocumentContext
+    ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        });
+        })
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: [
@@ -32,9 +32,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>,
         ],
-      };
+      }
     } finally {
-      sheet.seal();
+      sheet.seal()
     }
   }
   render() {
@@ -46,6 +46,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
